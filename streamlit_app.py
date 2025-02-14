3fef996754b95ff60eca4c961c6282a5216533b8
melandpri <59168892+melandpri@users.noreply.github.com>
2025-02-14 16:04:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ if st.sidebar.button("predict",type="primary"):
   #convertir mes données recupérer de l'utilisateur en tableau
 features = np.array([long_sepal,larg_sepal,long_petal,larg_petal])
 #ajout des titre de colonnes
-X = pd.DataFrame(features,columns=["SepalLength","SepalWidth","PetalLength","PetalWidth"])
+X =pd.DataFrame(features, columns=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'])
 #on normalise les données que le user a entré 
