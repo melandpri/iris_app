492745788f4f997ee8263836f2bc6bd6ffeb71b3
melandpri <59168892+melandpri@users.noreply.github.com>
2025-02-14 13:36:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ st.write ("Bienvenue sur mon jeu de données Iris ")
 
 st.sidebar.slider("longueur du sépale ",0.0,10.0,)
 
-if st.button("mon_bouton",type="Predict"):
+if st.button("mon_bouton",type="primary"):
   st.write("result")
 st.write("mon app")
