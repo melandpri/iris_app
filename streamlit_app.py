9055dd37c2bb51df53985b81cebf4e3d34ea8555
melandpri <59168892+melandpri@users.noreply.github.com>
2025-02-14 16:16:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,14 +14,14 @@ larg_sepal = st.sidebar.slider("largeur du sepal ",0.0,10.0)
 
 if st.sidebar.button("predict",type="primary"):
   modele = joblib.load("iris_modele.pk1")
-  scale = joblib.load("normaliser_data.pk1")
+  normalise = joblib.load("normaliser_data.pk1")
   #convertir mes données recupérer de l'utilisateur en tableau
   features = np.array([[long_sepal,larg_sepal,long_petal,larg_petal]])
   #ajout des titre de colonnes
   X =pd.DataFrame(features, columns=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'])
   #on normalise les données que le user a entré 
-  X_transform = scale.transform(X)
+  X_transform = normalise.transform(X)
   #je predicte mes données 
   prediction = modele.predict(X_transform)
-reponse_modele = prediction[0]
-st.write("votre fleur: ",reponse_modele)
+  reponse_modele = prediction[0]
+  st.write("votre fleur: ",reponse_modele)
