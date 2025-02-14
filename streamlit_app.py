0cc45b4c6e34440e894654c11508fc14282e9079
melandpri <59168892+melandpri@users.noreply.github.com>
2025-02-14 13:21:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.write ("Bienvenue sur mon jeu de données Iris ")
 
-if st.button("mon_bouton"):
+if st.button("mon_bouton",type="primary"):
   st.write("result")
 st.write("mon app")
