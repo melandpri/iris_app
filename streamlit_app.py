540d8fccf002e8b133d6bfecd43f6d4064eed898
melandpri <59168892+melandpri@users.noreply.github.com>
2025-02-14 13:51:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 st.write ("Bienvenue sur mon jeu de données Iris ")
 
 st.sidebar.slider("longueur du petal ",0.0,10.0)
+st.sidebar.slider("largeur du petal ",0.0,10.0)
+st.sidebar.slider("longueur du sepal ",0.0,10.0)
+st.sidebar.slider("largeur du sepal ",0.0,10.0)
+
 
 
 if st.button("mon_bouton",type="primary"):
